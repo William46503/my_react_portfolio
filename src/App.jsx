50955945f7681f00feabc78bc3a38014e9c0e46b
William46503 <52475057+William46503@users.noreply.github.com>
2020-11-14 14:12:26 +0900

    20201114- Commented: footer Changed: Project component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <Route path="/project" component={Project} />
             <Route path="/about" component={About} />
           </Switch>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </div>
     </BrowserRouter>
