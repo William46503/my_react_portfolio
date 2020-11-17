4716ec1e8796c43e14046afd2fb2543483e7ff27
William lu <williamlu46503@outlook.jp>
2020-11-17 22:49:48 +0900

    Changed to flexbox problem: Nav bar gets squished

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import Footer from "./Footer";
 // import Project_head_container from "../Component/Project-head-container";
 // import Thumbnail from "./Thumbnail";
 
 function Project() {
   return (
-    <div className="grid-parent project-top-container">
-      <div className="project-img">
-        <h2>Graphic Design</h2>
-        <img src="/images/GraphicDesign.jpg" alt="Picture of Graphic Design" />
+    <div className="project-main-maintainer">
+      <div className="project-top-container">
+        <a className="category-item" href="#">
+          <picture>
+            <img src="images\GraphicDesign.jpg" alt="GraphicDesignPicture"/>
+            
+          </picture>
+          <h2 className="category-title ">Graphic Design</h2>
+        </a>
+        <a className="category-item" href="#">
+        <picture>
+            <img src="images\Programming.jpg" alt="ProgrammingPicture"/>
+          </picture>
+          <h2 className="category-title">Programming</h2>
+        </a>
+        <a  className="category-item" href="#">
+        <picture>
+            <img src="images\MovieEdit.jpg" alt="MovieEditPicture"/>
+            
+          </picture>
+          {/* <h2 className="category-title">Video Content</h2> */}
+          </a>
       </div>
-      <div className="project-img">
-        <h2>Programming</h2>
-        <img src="/images/Programming.jpg" alt="Picture of Programming" />
-      </div>
-      <div className="project-img">
-        <h2>Video Editing/Other</h2>
-        <img src="/images/MovieEdit.jpg" alt="Picture of MovieEdit" />
-      </div>
+      <section>
+
+      </section>
+      {/* <Footer /> */}
     </div>
   );
 }
