483880443c262b60ff0db42f88cb33c88dde183f
William lu <williamlu46503@outlook.jp>
2020-11-19 22:53:50 +0900

    Fixed: - Projectjsx navbar squish problem(by removing the extra top-container) Added: -Category title and images -linear-gradient to act as a filter for category images

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,39 +1,41 @@
-import React from "react";
-import Footer from "./Footer";
+import React from 'react';
+import Footer from './Footer';
 // import Project_head_container from "../Component/Project-head-container";
 // import Thumbnail from "./Thumbnail";
 
 function Project() {
-  return (
-    <div className="project-main-maintainer">
-      <div className="project-top-container">
-        <a className="category-item" href="#">
-          <picture>
-            <img src="images\GraphicDesign.jpg" alt="GraphicDesignPicture"/>
-            
-          </picture>
-          <h2 className="category-title ">Graphic Design</h2>
-        </a>
-        <a className="category-item" href="#">
-        <picture>
-            <img src="images\Programming.jpg" alt="ProgrammingPicture"/>
-          </picture>
-          <h2 className="category-title">Programming</h2>
-        </a>
-        <a  className="category-item" href="#">
-        <picture>
-            <img src="images\MovieEdit.jpg" alt="MovieEditPicture"/>
-            
-          </picture>
-          {/* <h2 className="category-title">Video Content</h2> */}
-          </a>
-      </div>
-      <section>
-
-      </section>
-      {/* <Footer /> */}
-    </div>
-  );
+	return (
+		<div className="project-wrapper">
+			<div className="category-item">
+				<a href="#">
+					<h2>Graphic Design</h2>
+					<picture>
+						<img
+							className="category-image ci1"
+							src="/images/GraphicDesign.jpg"
+							alt="GraphicDesignPicture"
+						/>
+					</picture>
+				</a>
+			</div>
+			<div className="category-item">
+				<a href="#">
+					<h2>Programming</h2>
+					<picture>
+						<img className="category-image ci2" src="/images/Programming.jpg" alt="ProgrammingPicture" />
+					</picture>
+				</a>
+			</div>
+			<div className="category-item">
+				<a href="#">
+					<h2>Video Editing</h2>
+					<picture>
+						<img className="category-image ci3" src="/images/MovieEdit.jpg" alt="MovieEditingPicture" />
+					</picture>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Project;
