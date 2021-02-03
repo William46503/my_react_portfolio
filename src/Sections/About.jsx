7efdf49bdc05692f524c891ac64781f860d9b1af
William <williamlu46503@outlook.jp>
2021-02-03 21:24:52 +0900

    About-me-section-changes
    Added:
    -Clip about me title
    -add example sections

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../Styles/Constant.scss';
+import { Header, Icon } from 'semantic-ui-react';
 
 function About(props) {
 	return (
 		<div className="about-mainContainer">
-			<h1 className="about-title">Hello About</h1>
+			<h1 className="about-title">About Me</h1>
+			<div className="about-subContainer">
+				<section id="bio" className="about-sub-section">
+					<h2 type="title">
+						<span className="inner-text">Who Am I</span>
+					</h2>
+					<hr />
+					<p type="sub" className="description">
+						Redux, Storybook, Apollo (GraphQL), Styled Components, NextJS, Testing-Library, Jest, HTML, CSS,
+						and
+					</p>
+				</section>
+				<section id="programming" className="about-sub-section">
+					<h2 type="title">
+						<span className="inner-text">Who Am I</span>
+					</h2>
+					<hr />
+					<p type="sub" className="description">
+						Redux, Storybook, Apollo (GraphQL), Styled Components, NextJS, Testing-Library, Jest, HTML, CSS,
+						and
+					</p>
+				</section>
+			</div>
 		</div>
 	);
 }
