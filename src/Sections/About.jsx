bd9856136b5ad1c24639d1ae26fec52fa89bcfe7
William <williamlu46503@outlook.jp>
2021-02-08 22:40:36 +0900

    About Content & TimeLine
    Added :
    - About categories and content
    - TimeLine
    
    Next;
    - Style the timeline and add content to it

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,32 +1,71 @@
 import React from 'react';
 import '../Styles/Constant.scss';
-import { Header, Icon } from 'semantic-ui-react';
 
 function About(props) {
 	return (
 		<div className="about-mainContainer">
 			<h1 className="about-title">About Me</h1>
-			<div className="about-subContainer">
-				<section id="bio" className="about-sub-section">
-					<h2 type="title">
-						<span className="inner-text">Who Am I</span>
-					</h2>
+			<div className="about-subContainer-left">
+				<section className="about-sub-section">
+					<h2 type="title">Who Am I</h2>
 					<hr />
 					<p type="sub" className="description">
-						Redux, Storybook, Apollo (GraphQL), Styled Components, NextJS, Testing-Library, Jest, HTML, CSS,
-						and
+						A multilingual, linguist, hobby full-stack, game developer, gamer, fighting game nerd, WTF
+						Tae-Kwon-Do partitioner, recruiter & chef, from Norway.
 					</p>
 				</section>
-				<section id="programming" className="about-sub-section">
-					<h2 type="title">
-						<span className="inner-text">Who Am I</span>
-					</h2>
+				<section className="about-sub-section">
+					<h2 type="title">What Can I do</h2>
 					<hr />
 					<p type="sub" className="description">
-						Redux, Storybook, Apollo (GraphQL), Styled Components, NextJS, Testing-Library, Jest, HTML, CSS,
-						and
+						<ul className="listOfAbilities">
+							<li>
+								<span className="categoryName">Language:</span>
+								<ul className="languageList">
+									<li>
+										<span role="img">🇨🇭</span> Mandarin - Fluent
+									</li>
+									<li>
+										<span role="img">🇭🇰</span> Cantonese - Fluent
+									</li>
+									<li>
+										<span role="img">🇺🇸</span> English - Fluent
+									</li>
+									<li>
+										<span role="img">🇯🇵</span> Japanese - Fluent
+									</li>
+									<li>
+										<span role="img">🇳🇴</span>Norwegian - Fluent
+									</li>
+								</ul>
+							</li>
+							<li>
+								<span className="categoryName">Programming(Learning):</span> JavaScript(React, Node),
+								Flutter, UE/C++, MaterialUI, SemanticUI
+							</li>
+							<li>
+								<span className="categoryName">Graphic Design:</span> AdobeXD, Photoshop, Gimp
+							</li>
+							<li>
+								<span className="categoryName">Video Editing:</span> Sony Vegas, Filmora
+							</li>
+						</ul>
 					</p>
 				</section>
+			</div>
+			<div class="timeline">
+				<div class="container left">
+					<div class="content">
+						<h2>2017</h2>
+						<p>Lorem ipsum..</p>
+					</div>
+				</div>
+				<div class="container left">
+					<div class="content">
+						<h2>2016</h2>
+						<p>Lorem ipsum..</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
