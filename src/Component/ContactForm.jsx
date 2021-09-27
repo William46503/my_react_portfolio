c2a3ba0c33542a2fea47ade752879ee273d14496
William46503 <52475057+William46503@users.noreply.github.com>
2021-09-27 22:24:00 +0900

    contactform-info-completed Next: contact section Styling

--- src/Component/ContactForm.jsx
+++ src/Component/ContactForm.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import {Form} from 'semantic-ui-react';
+import React from "react";
+import { Form, Input, TextArea } from "semantic-ui-react";
 
-function ContactForm()
-{
-    return(
-        <Form>
-        <Form.Group widths={5}>
-          <Form.Input fluid label='Your Name' placeholder='Jordon Smith' />
+function ContactForm() {
+  return (
+    <Form className="form-container">
+      <Form.Group className="general-input-wrapper">
+       <Form.Group className="general-input">
+        <Form.Input
+          fluid
+          label="Your Name"
+          placeholder="Jordon Smith"
+          control={Input}
+        />
+        <Form.Input
+          id="form-input-email"
+          label="Email"
+          placeholder="watisuraddress@gmail.com"
+          control={Input}
+        />
+        <Form.Input label="Phone" placeholder="xxxxxxxxxxx" control={Input}/>
         </Form.Group>
-        <Form.TextArea label='About' placeholder='Tell us more about you...' />
-        <Form.Button>Submit</Form.Button>
-      </Form>
-    )
-}
+       
+        <Form.TextArea label="Message" rows={10} placeholder='Your Message' />
+      </Form.Group>
 
+      <Form.Button className="submit-button">Talk to you soon!</Form.Button>
+    </Form>
+  );
+}
 
 export default ContactForm;
--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Icon} from "semantic-ui-react"
 
 import ContactForm from '../Component/ContactForm.jsx';
 
@@ -7,6 +8,11 @@ export default function Contact() {
 		<div className="contact-container">
 			<span id="contact-section" />
 			<h1>Hello, this is contact</h1>
+			<section className="contact-my-info">
+				<section><Icon name="map marker alternate"/><p>Tokyo Meguro</p></section>
+				<section><Icon name="phone"/><p>070-4438-5505</p></section>
+				<section><Icon name="at"/><p>williamlu46503@outlook.jp</p></section>
+			</section>
 			<ContactForm/>
 		</div>
 	);
