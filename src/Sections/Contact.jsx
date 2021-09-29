840a6ee998eb2f52918a75554314a7a3c9acf0aa
William46503 <52475057+William46503@users.noreply.github.com>
2021-09-29 22:35:06 +0900

    Styled-prototype-ContactSection

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -7,11 +7,11 @@ export default function Contact() {
 	return (
 		<div className="contact-container">
 			<span id="contact-section" />
-			<h1>Hello, this is contact</h1>
+			<h1>Get in touch</h1>
 			<section className="contact-my-info">
-				<section><Icon name="map marker alternate"/><p>Tokyo Meguro</p></section>
-				<section><Icon name="phone"/><p>070-4438-5505</p></section>
-				<section><Icon name="at"/><p>williamlu46503@outlook.jp</p></section>
+				<section><Icon size='big' name="map marker alternate"/><p>Tokyo Meguro</p></section>
+				<section><Icon size='big' name="phone"/><p>070-4438-5505</p></section>
+				<section><Icon size='big' name="at"/><p>williamlu46503@outlook.jp</p></section>
 			</section>
 			<ContactForm/>
 		</div>
