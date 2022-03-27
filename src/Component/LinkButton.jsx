6f5b4073da1aed5d74cc2ff05fd02e123f77683f
William46503 <52475057+William46503@users.noreply.github.com>
2022-03-27 16:45:39 +0900

    Immigrate rails-portfolio styles

--- src/Component/LinkButton.jsx
+++ src/Component/LinkButton.jsx
@@ -5,7 +5,7 @@ function LinkButton(props) {
 	return (
 		<div className={props.buttonContainerClass}>
 			<a href="https://www.linkedin.com/in/william-l-w-lu-689167184" target="blank">
-				<Button className={props.buttonClass} size="huge" animated="fade">
+				<Button className={props.buttonClass} size="medium" animated="fade">
 					<Button.Content hidden className={props.hiddenIcon}>
 						Linkedin <Icon name="arrow alternate circle right outline" size="small" />
 					</Button.Content>
@@ -15,7 +15,7 @@ function LinkButton(props) {
 				</Button>
 			</a>
 			<a href="https://github.com/William46503" target="blank">
-				<Button className={props.buttonClass} size="huge" animated="fade">
+				<Button className={props.buttonClass} size="medium" animated="fade">
 					<Button.Content hidden className={props.hiddenIcon}>
 						Github <Icon name="arrow alternate circle right outline" size="small" />
 					</Button.Content>
@@ -25,7 +25,7 @@ function LinkButton(props) {
 				</Button>
 			</a>
 			<a href="https://twitter.com/NextLvLPenguin" target="blank">
-				<Button className={props.buttonClass} size="huge" animated="fade">
+				<Button className={props.buttonClass} size="medium" animated="fade">
 					<Button.Content hidden className={props.hiddenIcon}>
 						Twitter <Icon name="arrow alternate circle right outline" size="small" />
 					</Button.Content>
